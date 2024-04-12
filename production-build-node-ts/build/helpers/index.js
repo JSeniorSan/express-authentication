99c2a38f2367e64f852d60a8847652c5c3f53f57
Tekai_ <vip007_007@mail.ru>
2024-04-13 01:46:44 +0300

    initial with db commit

---
+++ production-build-node-ts/build/helpers/index.js
@@ -0,0 +1,6 @@
+import crypto from 'crypto';
+const SECRET = 'TEKAI-REST-API';
+export const random = () => crypto.randomBytes(128).toString('base64');
+export const authentication = (salt, password) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
+};
